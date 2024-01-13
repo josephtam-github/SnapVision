d8a4a6301264a7758b9ad7e0d19758aa8d097e9a
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-01-13 21:27:59 +0100

    fix: use inception preprocessing function and target size

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import numpy as np
 from tensorflow import keras
 from keras.applications.inception_v3 import InceptionV3
 import keras.utils as image
-from keras.applications.resnet import preprocess_input, decode_predictions
+from keras.applications.inception_v3 import preprocess_input, decode_predictions
 
 
 def load_model():
@@ -12,8 +12,8 @@ def load_model():
 
 
 def preprocess_image(filename):
-    # loads the image and resizes it to (224, 224). ResNet input requirement
-    img = image.load_img(filename, target_size=(224, 224))
+    # loads the image and resizes it to (224, 224). InceptionV3 input requirement
+    img = image.load_img(filename, target_size=(299, 299))
     # converts the image to a NumPy
     x = image.img_to_array(img)
     # adds a batch dimension as most models expect a 4D tensor for prediction
