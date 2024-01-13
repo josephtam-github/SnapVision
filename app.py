b34c7aeeb0d0b905f5a4879010fed78e57881ba1
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-01-13 19:55:33 +0100

    feat: render home page

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
- 
+from flask import Flask, render_template
+
 app = Flask(__name__)
- 
-@app.route('/')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
