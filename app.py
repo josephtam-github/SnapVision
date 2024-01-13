8883dfd95866def79a3664028ce60be6ff04f567
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-01-13 20:07:17 +0100

    feat: create file upload route

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(filename)
+
+    return "Error"
 
 if __name__ == "__main__":
     app.run(debug=True)
